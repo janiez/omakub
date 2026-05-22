70e6e78239ba45d73eea00c10785faf1951ed1c6
Jorge-Anez <jorge.anez@coxautoinc.com>
2026-05-22 18:20:40 -0400

    Update LocalSend installation to use tar.gz for arm64 support

--- install/desktop/app-localsend.sh
+++ install/desktop/app-localsend.sh
@@ -2,7 +2,8 @@
 
 cd /tmp
 LOCALSEND_VERSION=$(curl -s "https://api.github.com/repos/localsend/localsend/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
-wget -O localsend.deb "https://github.com/localsend/localsend/releases/latest/download/LocalSend-${LOCALSEND_VERSION}-linux-arm64.deb"
-sudo apt install -y ./localsend.deb
-rm localsend.deb
+wget -O localsend.tar.gz "https://github.com/localsend/localsend/releases/latest/download/LocalSend-${LOCALSEND_VERSION}-linux-arm-64.tar.gz"
+tar -xf localsend.tar.gz
+sudo install -Dm755 LocalSend-${LOCALSEND_VERSION}-linux-arm-64 /usr/local/bin/localsend
+rm -f localsend.tar.gz LocalSend-${LOCALSEND_VERSION}-linux-arm-64
 cd -
