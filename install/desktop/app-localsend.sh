e45628dd3cef1d4beb9dbb41dfe5c59ba9e45478
Jorge-Anez <jorge.anez@coxautoinc.com>
2026-05-22 19:08:13 -0400

    Extract LocalSend installation to a subdirectory and update installation path

--- install/desktop/app-localsend.sh
+++ install/desktop/app-localsend.sh
@@ -3,7 +3,7 @@
 cd /tmp
 LOCALSEND_VERSION=$(curl -s "https://api.github.com/repos/localsend/localsend/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
 wget -O localsend.tar.gz "https://github.com/localsend/localsend/releases/latest/download/LocalSend-${LOCALSEND_VERSION}-linux-arm-64.tar.gz"
-tar -xf localsend.tar.gz
-sudo install -Dm755 LocalSend-${LOCALSEND_VERSION}-linux-arm-64 /usr/local/bin/localsend
-rm -f localsend.tar.gz LocalSend-${LOCALSEND_VERSION}-linux-arm-64
+tar -xf localsend.tar.gz --one-top-level="localsend"
+sudo install -Dm755 localsend/localsend_app /usr/local/bin/localsend
+rm -rf localsend.tar.gz localsend
 cd -
