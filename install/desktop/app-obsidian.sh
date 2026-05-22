be08d4e6dc777f9d6c4ee39bf9e196f61df4268e
Jorge Anez <jorge.aniez@outlook.com>
2026-05-22 19:29:33 -0400

    add obsidian arm64

--- install/desktop/app-obsidian.sh
+++ install/desktop/app-obsidian.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Obsidian is a multi-platform note taking application. See https://obsidian.md
-cd /tmp
+#cd /tmp
 OBSIDIAN_VERSION=$(curl -s https://api.github.com/repos/obsidianmd/obsidian-releases/releases/latest | grep -Po '"tag_name": "v\K[^"]*')
 wget -O obsidian.tar.gz "https://github.com/obsidianmd/obsidian-releases/releases/download/v${OBSIDIAN_VERSION}/obsidian-${OBSIDIAN_VERSION}-arm64.tar.gz"
-tar -xf obsidian.tar.gz --one-top-level="obsidian"
-sudo install -Dm755 obsidian/obsidian /usr/local/bin/obsidian
-rm -rf obsidian.tar.gz obsidian
+tar -xf obsidian.tar.gz
+sudo install -Dm755 obsidian-${OBSIDIAN_VERSION}-arm64/obsidian /usr/local/bin/obsidian
+rm -rf obsidian.tar.gz obsidian-${OBSIDIAN_VERSION}-arm64
 cd -
