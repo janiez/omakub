3ef2887c1ca0ac40b56da9e65a6874d1f67fdc41
Jorge Anez <jorge.aniez@outlook.com>
2026-06-14 22:22:00 -0400

    fix spotify

--- migrations/1770656330.sh
+++ migrations/1770656330.sh
@@ -4,5 +4,5 @@
 if [ -f /etc/apt/sources.list.d/spotify.list ]; then
   echo "Updating Spotify GPG key..."
   curl -sS https://download.spotify.com/debian/pubkey_5384CE82BA52C83A.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
-  echo "deb [signed-by=/etc/apt/trusted.gpg.d/spotify.gpg] https://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+  echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/spotify.gpg] https://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 fi
--- uninstall/app-spotify.sh
+++ uninstall/app-spotify.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 sudo apt remove -y spotify-client
+sudo rm -f /etc/apt/sources.list.d/spotify.list
+sudo rm -f /etc/apt/trusted.gpg.d/spotify.gpg
